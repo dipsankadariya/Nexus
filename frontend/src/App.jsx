9afbff023110b34c5f1d4902ee1c8018fe9f00d1
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-12 22:39:29 +0545

    Routing Configuration added and Signup Page UI Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/auth/login/LoginPage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div>App</div>
+    <div className='flex max-w-6xl mx-auto'>
+      <Routes>
+        <Route path='/' element= {<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+      </Routes>
+    </div>
   )
 }
 
