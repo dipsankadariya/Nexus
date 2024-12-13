ff4a68f6eda807943177f8db98f563d065277f40
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-13 14:05:31 +0545

    Added HomePage, SideBar,and More

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,12 @@ import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import { Routes, Route } from 'react-router-dom';
+import Sidebar from './components/common/Sidebar';
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+      {/**common components*/}
+      <Sidebar/>
       <Routes>
         <Route path='/' element= {<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>} />
@@ -16,3 +19,5 @@ function App() {
 }
 
 export default App;
+
+//5:28
--- frontend/src/components/svgs/X.jsx
+++ frontend/src/components/svgs/X.jsx
@@ -3,13 +3,13 @@ const XSvg = (props) => (
 	  version="1.1"
 	  id="Layer_1"
 	  xmlns="http://www.w3.org/2000/svg"
-	  xmlns:xlink="http://www.w3.org/1999/xlink"
+	  xmlnsXlink="http://www.w3.org/1999/xlink"
 	  x="0px"
 	  y="0px"
 	  width="100%"
 	  viewBox="0 0 500 500"
-	  enable-background="new 0 0 500 500"
-	  xml:space="preserve"
+	  enableBackground="new 0 0 500 500"
+	  xmlSpace="preserve"
 	  {...props}
 	>
 	  <path
@@ -34,4 +34,3 @@ const XSvg = (props) => (
   );
   
   export default XSvg;
-  
